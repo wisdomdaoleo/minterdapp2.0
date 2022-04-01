9ded4fde9ee7702388159fe0541b5987899dda7e
wisdomdaoleo <120478814liulei@gmail.com>
2022-04-01 23:18:15 +0800

    minterdapp2.0_npm+i_8830

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 100,
     layersOrder: [
       { name: "Background" },
       { name: "Clothes" },
--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x9dE78833b8Fe2CcBC53020F63c2B4C8d2c758830"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
